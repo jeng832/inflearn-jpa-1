49f70f3580bab814ae7295d53bf292533d2f88d5
jeng832 <jeng832@gmail.com>
2020-12-18 16:46:56 +0900

    주문, 주문상품 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -66,4 +66,25 @@ public class Order {
         order.setOrderDate(LocalDateTime.now());
         return order;
     }
+
+    // biz logic
+    // 주문 취소
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 붉가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    // 조회 로직
+    // 전체 주문 가격 조회
+    public int getTotalPrice() {
+        return orderItems.stream()
+                .mapToInt(OrderItem::getTotalPrice)
+                .sum();
+    }
 }
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -24,4 +24,23 @@ public class OrderItem {
 
     private int orderPrice;
     private int count;
+
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+    // biz logic
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    // 조회 로직:
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
